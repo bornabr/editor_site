8c283a43c7b1fb448c16047846d483e66e24cb88
Borna <bornabarahimi@gmail.com>
2019-02-13 07:44:07 +0330

    Final version

--- editor/views.py
+++ editor/views.py
@@ -2,7 +2,6 @@ import os
 
 from PIL import Image
 from django.conf import settings
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 from .models import Post
@@ -14,13 +13,11 @@ def home(request):
 
 def upload_img(request):
     if request.method == 'POST':
-        # if request.FILES.get('imgToUpload', None):
 
-        # try:
-        img = request.FILES['imgToUpload']
-        # except:
-            # return render(request, 'editor/home.html')
-            # return redirect('/')
+        try:
+            img = request.FILES['imgToUpload']
+        except:
+            return redirect('/')
         post = Post(name=img.name, image=img)
         request.session['new_post'] = post
         if not request.session.session_key:
@@ -41,9 +38,6 @@ def edit(request):
         im = Image.open(os.path.join(settings.MEDIA_ROOT, post.name))
         new_im = None
         if request.POST.get('crop'):
-            # print('crop')
-            # if request.POST.get("crop-left", None) and request.POST.get("crop-upper", None) and request.POST.get(
-            #         "crop-right", None) and request.POST.get("crop-lower", None):
             try:
                 box = (int(request.POST["crop-left"]), int(request.POST["crop-upper"]),
                        int(request.POST["crop-right"]), int(request.POST["crop-lower"]))
@@ -55,8 +49,6 @@ def edit(request):
                 return render(request, 'editor/edit.html', context=context)
             new_im = crop(im, box)
         elif request.POST.get('resize'):
-            # print('resize')
-            # if request.POST.get("resize-height", None) and request.POST.get("resize-width", None):
             try:
                 size = (int(request.POST["resize-height"]), int(request.POST["resize-width"]))
             except:
@@ -67,8 +59,6 @@ def edit(request):
                 return render(request, 'editor/edit.html', context=context)
             new_im = resize(im, size)
         elif request.POST.get('rotate'):
-            # print('rotate')
-            # if request.POST.get("rotate-angle", None):
             try:
                 new_im = rotate(im, int(request.POST["rotate-angle"]))
             except:
@@ -78,7 +68,6 @@ def edit(request):
                 }
                 return render(request, 'editor/edit.html', context=context)
         elif request.POST.get('BAW'):
-            # print('BAW')
             new_im = black_and_white(im)
         elif request.POST.get('done'):
             post.delete()
@@ -87,8 +76,6 @@ def edit(request):
             post.is_share = True
             post.save()
             return render(request, 'editor/share.html', context={'title': 'Shared Photos', 'posts': Post.objects.all()})
-            # return redirect('../share')
-            # return HttpResponseRedirect('../share')
         new_im.save(post.image.path, format='PNG')
         request.session['new_post'] = post
         context = {
